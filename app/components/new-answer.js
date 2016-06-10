5817f697da5fa1ef34b7aaa063efa81a57fa7cc9
ashleyraymaceli <ashley.maceli@gmail.com>
2016-06-10 15:14:15 -0700

    add pick avatar actions to new-answer component

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
+var clearAvatarChoice = function() {
+  $("#pickSquare").removeClass("selected-avatar");
+  $("#pickDiamond").removeClass("selected-avatar");
+  $("#pickPyramid").removeClass("selected-avatar");
+  $("#pickCat").removeClass("selected-avatar");
+}
+
 export default Ember.Component.extend({
   actions: {
     saveAnswer() {
@@ -10,6 +17,26 @@ export default Ember.Component.extend({
         question: this.get('question')
       };
       this.sendAction('saveAnswer', params);
+    },
+    pickSquare() {
+      clearAvatarChoice();
+      this.set('image', "https://67.media.tumblr.com/36f3f6b6b1699e13bb63c042a53b6fff/tumblr_moa11zV41c1ru83xzo1_500.png");
+      $("#pickSquare").addClass("selected-avatar");
+    },
+    pickDiamond() {
+      clearAvatarChoice();
+      this.set('image', "https://66.media.tumblr.com/9f9b498bf798ef43dddeaa78cec7b027/tumblr_o51oavbMDx1ugpbmuo7_500.png");
+      $("#pickDiamond").addClass("selected-avatar");
+    },
+    pickPyramid() {
+      clearAvatarChoice();
+      this.set('image', "http://67.media.tumblr.com/75bad14fee104f69652084d545213291/tumblr_mldfty8fh41qcnibxo5_540.png");
+      $("#pickPyramid").addClass("selected-avatar");
+    },
+    pickCat() {
+      clearAvatarChoice();
+      this.set('image', "https://66.media.tumblr.com/7d65a925636d6e3df94e2ebe30667c29/tumblr_nq1zg0MEn51qg6rkio1_500.jpg");
+      $("#pickCat").addClass("selected-avatar");
     },
   }
 });
