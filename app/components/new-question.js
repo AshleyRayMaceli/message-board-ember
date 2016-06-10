65e4e513e6548be878609e43583e45e6e851aaff
ashleyraymaceli <ashley.maceli@gmail.com>
2016-06-10 10:13:44 -0700

    add saveQuestion method and pass it through new-question component to index route

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  actions: {
+    saveQuestion() {
+      var params = {
+        asked: this.get('asked'),
+        author: this.get('author'),
+        notes: this.get('notes'),
+        avatar: this.get('avatar'),
+      };
+      this.sendAction('saveQuestion', params);
+      this.set('asked', "");
+      this.set('author', "");
+      this.set('notes', "");
+      this.set('avatar', "");
+    }
+  }
 });
