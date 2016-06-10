6a531cf27cafbddcbbe65930ba72a4b1a6efd670
ashleyraymaceli <ashley.maceli@gmail.com>
2016-06-10 11:41:10 -0700

    add new-answer component with forms and update models for one-to-many relationship

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   comment: DS.attr(),
-  author: DS.attr()
+  author: DS.attr(),
+  avatar: DS.attr(),
+  question: DS.belongsTo('question', { async: true })
 });
--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   asked: DS.attr(),
   author: DS.attr(),
   notes: DS.attr(),
-  avatar: DS.attr()
+  avatar: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
 });
